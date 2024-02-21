d8253edca713e4b1027234e3b835c75384dc7f62
Alexxx <2323426610@qq.com>
2024-02-22 01:26:00 +0800

    frame is built

---
+++ calendarGeneration.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+bool is_leap(const int &year)
+{
+    return true;
+}
+
+int dayofmonth(const int &y, const int &month)
+{
+    return 1;
+}
+
+void printCalendar(const int &year, const int &month, int day = 1)
+{
+}
+
+int mian()
+{
+    return 0;
+}
